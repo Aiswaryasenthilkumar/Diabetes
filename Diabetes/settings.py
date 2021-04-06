6ea3a3f8837690f6fb1ae2a801892f994cd08b3b
Aiswaryasenthilkumar <ishusenthil28042gmail.com>
2021-04-06 13:24:00 +0530

    edited

--- Diabetes/settings.py
+++ Diabetes/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '0z_7+_-y3k_&!219(e*ol0$ecppn3iojm4i%@s+)^-ub^3mdyu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.23.63.83','localhost']
+ALLOWED_HOSTS = ['18.191.21.85','localhost']
 
 
 # Application definition
